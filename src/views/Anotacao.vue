848bd0de7b0fb9b70f74cbcdd4265c606cb6b95c
giovani <giovanicarvalhoavila@hotmail.com>
2025-06-01 20:20:16 -0300

    add cadastro

--- src/views/Anotacao.vue
+++ src/views/Anotacao.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <p>texto: <input type="text" v-model="texto" required /></p>
+    <p>data hora: <input type="datetime-local" v-model="dataHora" /></p>
+    <p required>
+      usuarios:
+      <select name="usuarios" id="" v-model="idUsuario">
+        <option v-for="(usuario, index) in usuarios" :key="index" :value="usuario.id">
+          {{ usuario.nome }}
+        </option>
+      </select>
+    </p>
+    <p required>idUsario : <input type="number" v-model="idUsuario" /></p>
+    <button @click="incluir">Cadastrar</button>
+    <p v-if="erro">{{ erro }}</p>
     <table>
       <thead>
         <tr>
@@ -13,7 +26,7 @@
         <tr v-for="(anotacao, index) in anotacoes" :key="index">
           <td>{{ anotacao?.id }}</td>
           <td>{{ anotacao?.texto }}</td>
-          <td>{{ calcularMesesDesde(anotacao?.dataHora ?? "")}}</td>
+          <td>{{ calcularMesesDesde(anotacao?.dataHora ?? '') }} meses</td>
           <td>{{ anotacao?.usuario.nome }}</td>
         </tr>
       </tbody>
@@ -26,8 +39,9 @@ import { ref } from 'vue'
 import axios from 'axios'
 import { onMounted } from 'vue'
 
-const nome = ref<string>('')
-const senha = ref<string>('')
+const texto = ref<string>('')
+const dataHora = ref<Date>()
+const idUsuario = ref<number>()
 const erro = ref<string>('')
 
 const anotacoes = ref([
@@ -87,15 +101,44 @@ function calcularMesesDesde(dataHora: string): number {
   return (anoAtual - anoInicial) * 12 + (mesAtual - mesInicial)
 }
 
-async function incluir() {
+interface usuario {
+  id?: number
+  nome: string
+  senha: string
+}
+
+const usuarios = ref<usuario[]>([
+  { id: 1, nome: 'admin', senha: '123' },
+  { id: 2, nome: 'teste', senha: 'abc' },
+])
+
+async function buscarUsuarios() {
   try {
-    await axios.post('usuario', {
-      nome: nome.value,
-      senha: senha.value,
+    usuarios.value = (await axios.get('usuario')).data
+    erro.value = ''
+  } catch (e) {
+    erro.value = (e as Error).message
+  }
+}
+
+async function incluir() {
+  if (!texto.value || !dataHora.value || !idUsuario.value) {
+    erro.value = 'Preencha todos os campos obrigatórios.'
+    return
+  }
+
+  try {
+    await axios.post('anotacao', {
+      texto: texto.value,
+      dataHora: dataHora.value,
+      usuario: {
+        id: idUsuario.value,
+      },
     })
     erro.value = ''
-    nome.value = ''
-    senha.value = ''
+    texto.value = ''
+    dataHora.value = undefined
+    idUsuario.value = 0
     atualizar()
   } catch (e) {
     erro.value = (e as Error).message
@@ -104,5 +147,6 @@ async function incluir() {
 
 onMounted(() => {
   atualizar()
+  buscarUsuarios()
 })
 </script>
